e9ca33d244b7443ccf60d67df711d16bcd834caf
Kelvin Murilo <kelvinmuriloas@gmail.com>
2021-05-07 19:58:04 -0300

    shared module creation

---
+++ condomeasy-project-webapp/src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './components/dumbs/buttons/button/button.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ButtonComponent
+  ]
+})
+export class SharedModule { }
