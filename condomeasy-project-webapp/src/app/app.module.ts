2271aac5067c5dc1e55a0a4c29d76a3b4fb60af7
Kelvin Murilo <kelvinmuriloas@gmail.com>
2021-05-07 19:58:04 -0300

    shared module creation

--- condomeasy-project-webapp/src/app/app.module.ts
+++ condomeasy-project-webapp/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/* Components */
 import { AppComponent } from './app.component';
+
+/* Modules */
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
