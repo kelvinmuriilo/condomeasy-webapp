7a968ca274ba29698b6e15309fd8099e64eefdd9
Kelvin Murilo <kelvinmuriloas@gmail.com>
2021-05-17 20:32:36 -0300

    Auth guard config and fake authentication

--- condomeasy-project-webapp/src/app/app.routing.module.ts
+++ condomeasy-project-webapp/src/app/app.routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { Routes, RouterModule, CanActivate } from '@angular/router'; // CLI imports router
 import { HomeComponent } from './features/home/home.component';
 import { LoginComponent } from './features/login/login.component';
 import { SignupComponent } from './features/signup/signup.component';
+import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'signup',
