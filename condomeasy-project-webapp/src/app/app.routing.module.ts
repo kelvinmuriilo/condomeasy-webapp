abb5f6c1fa2286223339e15336003e51efa6b84e
Kelvin Murilo <kelvinmuriloas@gmail.com>
2021-05-17 20:07:19 -0300

    router config

--- condomeasy-project-webapp/src/app/app.routing.module.ts
+++ condomeasy-project-webapp/src/app/app.routing.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './features/login/login.component';
+import { SignupComponent } from './features/signup/signup.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        
-    }
-]; 
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- condomeasy-project-webapp/src/app/features/login/login.component.ts
+++ condomeasy-project-webapp/src/app/features/login/login.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from 'src/app/core/auth/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
@@ -17,7 +17,7 @@ export class LoginComponent implements OnInit {
     private authService: AuthService,
     private router: Router,
     private toastrService: ToastrService
-    ) { }
+  ) {}
 
   ngOnInit(): void {
     this.initLoginForm();
@@ -28,13 +28,12 @@ export class LoginComponent implements OnInit {
     let password = this.loginForm.value.password;
 
     if (this.loginForm.valid) {
-      this.authService.autenticate(username, password)
-      .subscribe(
+      this.authService.authenticate(username, password).subscribe(
         () => {
           this.router.navigate(['/home']);
         },
         () => {
-          this.toastrService.error('Falha ao realizar login')
+          this.toastrService.error('Falha ao realizar login');
         }
       );
     } else {
@@ -46,20 +45,17 @@ export class LoginComponent implements OnInit {
         this.setErrorOnFormControlWhenIsBlank('password');
       }
     }
-    
-    
   }
 
   private setErrorOnFormControlWhenIsBlank(formControlName: string): void {
-    this.loginForm.get(formControlName).setErrors({ required: true })
+    this.loginForm.get(formControlName).setErrors({ required: true });
     this.loginForm.get(formControlName).markAsTouched();
   }
 
   private initLoginForm(): void {
     this.loginForm = this.formBuilder.group({
       username: this.formBuilder.control('', Validators.required),
-      password: this.formBuilder.control('', Validators.required)
+      password: this.formBuilder.control('', Validators.required),
     });
   }
-
 }
