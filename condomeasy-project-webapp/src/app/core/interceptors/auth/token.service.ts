7a4f87cc4c82ee2dfe9decb3663c8215756c120a
Kelvin Murilo <kelvinmuriloas@gmail.com>
2021-05-16 22:13:15 -0300

    token service create

---
+++ condomeasy-project-webapp/src/app/core/interceptors/auth/token.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  constructor() { }
+
+  hasToken(): void {}
+
+  getToken(): void {}
+
+  setToken(): void {}
+
+}
