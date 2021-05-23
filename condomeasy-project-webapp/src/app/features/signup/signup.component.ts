b954d0245a084f60111ba396efce9aee61d2fd73
Kelvin Murilo <kelvinmuriloas@gmail.com>
2021-05-23 15:20:18 -0300

    signup form validations

--- condomeasy-project-webapp/src/app/features/signup/signup.component.ts
+++ condomeasy-project-webapp/src/app/features/signup/signup.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { FormValidations } from 'src/app/shared/custom-form-validations/custom.form-validations';
+import { SignupModel } from './signup.model';
+import { SignupService } from './signup.service';
 
 @Component({
   selector: 'app-signup',
@@ -15,7 +19,12 @@ export class SignupComponent implements OnInit {
     },
   };
 
-  constructor(private formbuilder: FormBuilder) {}
+  constructor(
+    private formbuilder: FormBuilder,
+    private signupService: SignupService,
+    private toastService: ToastrService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.startSignupForm();
@@ -23,7 +32,16 @@ export class SignupComponent implements OnInit {
 
   send(): void {
     if (this.signupForm.valid) {
-      console.log('valid');
+      this.signupService.send(this.getSignupModel()).subscribe(
+        () => {
+          this.toastService.success('Cadastro realizado com sucesso!');
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          this.toastService.error('Houve um erro ao tentar realizar cadastro.');
+        },
+        () => {}
+      );
     } else {
       Object.keys(this.signupForm.controls).forEach((key) => {
         if (!this.signupForm.get(key).value)
@@ -51,7 +69,7 @@ export class SignupComponent implements OnInit {
         ]),
         username: this.formbuilder.control('', [
           Validators.required,
-          Validators.minLength(4),
+          Validators.minLength(3),
         ]),
         password: this.formbuilder.control('', Validators.required),
         confirmPassword: this.formbuilder.control('', Validators.required),
@@ -63,7 +81,11 @@ export class SignupComponent implements OnInit {
           Validators.required,
           Validators.email,
         ]),
-        phone: this.formbuilder.control('', [Validators.required]),
+        phone: this.formbuilder.control('', [
+          Validators.required,
+          Validators.minLength(11),
+          Validators.maxLength(11),
+        ]),
       },
       {
         validators: FormValidations.passwordMatchValidator,
@@ -74,5 +96,21 @@ export class SignupComponent implements OnInit {
   private setErrorOnFormControlWhenIsBlank(formControlName: string): void {
     this.signupForm.get(formControlName).setErrors({ required: true });
     this.signupForm.get(formControlName).markAsTouched();
+  }
+
+  private getSignupModel(): SignupModel {
+    return {
+      nome: this.signupForm.value.firstName,
+      sobrenome: this.signupForm.value.lastName,
+      cpf: this.signupForm.value.cpf,
+      blocoApto: this.signupForm.value.blocoApto,
+      condominioId: 0,
+      email: this.signupForm.value.email,
+      numeroApto: this.signupForm.value.numeroApto,
+      perfilId: 0,
+      senha: this.signupForm.value.password,
+      telefone: this.signupForm.value.phone,
+      usuario: this.signupForm.value.username,
+    };
   }
 }
---
+++ condomeasy-project-webapp/src/app/features/signup/signup.model.ts
@@ -0,0 +1,13 @@
+export interface SignupModel {
+  nome: string;
+  blocoApto: string;
+  cpf: string;
+  usuario: string;
+  senha: string;
+  perfilId: number;
+  numeroApto: number;
+  condominioId: number;
+  email: string;
+  telefone: string;
+  sobrenome: string;
+}
