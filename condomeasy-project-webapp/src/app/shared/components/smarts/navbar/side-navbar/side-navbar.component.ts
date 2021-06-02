68f5b3eeb9409a5f223fc6c4a7779125e7d2c86c
Kelvin Murilo <kelvinmuriloas@gmail.com>
2021-06-02 20:50:11 -0300

    navbar style

--- condomeasy-project-webapp/src/app/shared/components/smarts/navbar/side-navbar/side-navbar.component.ts
+++ condomeasy-project-webapp/src/app/shared/components/smarts/navbar/side-navbar/side-navbar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { UserService } from 'src/app/core/user/user.service';
 
 @Component({
   selector: 'app-side-navbar',
   templateUrl: './side-navbar.component.html',
-  styleUrls: ['./side-navbar.component.css']
+  styleUrls: ['./side-navbar.component.css'],
 })
 export class SideNavbarComponent implements OnInit {
+  @Output() logoutEvent: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private matDialogRef: MatDialogRef<SideNavbarComponent>
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  logout(): void {
+    this.matDialogRef.close();
+    this.userService.logout();
   }
-
 }
