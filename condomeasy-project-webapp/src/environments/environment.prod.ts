e9c79f11fa52a52889dff609178f6afccf34e5a2
yagobrandao <yagobrandao20@gmail.com>
2021-11-04 22:45:47 -0300

    feat: cors

--- condomeasy-project-webapp/src/environments/environment.prod.ts
+++ condomeasy-project-webapp/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl:
     'https://condomeasy-bff-service-uat.herokuapp.com/condomeasy-backend-api/v1',
-  uploadImagesUrl: 'https://dev.katianeespacodabeleza.com.br/arquivos/save.php',
+  uploadImagesUrl: 'https://financas.yagobrandao.dev.br/upload',
 };
