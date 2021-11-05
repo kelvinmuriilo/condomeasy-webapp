6dc5826ca9ad9b485d37a5ef7e7521c4e14d4d57
yagobrandao <yagobrandao20@gmail.com>
2021-11-04 23:11:42 -0300

    change urç

--- condomeasy-project-webapp/src/environments/environment.prod.ts
+++ condomeasy-project-webapp/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl:
     'https://condomeasy-bff-service-uat.herokuapp.com/condomeasy-backend-api/v1',
-  uploadImagesUrl: 'https://financas.yagobrandao.dev.br/upload',
+  uploadImagesUrl: 'http://161.97.107.158:4444/anexar.php',
 };
